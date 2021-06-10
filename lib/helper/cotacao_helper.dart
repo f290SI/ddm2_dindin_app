30a62fb0f3d525753acb4d0f6a9f7c983c32f645
Esdras Bezerra da Silva <esdrasilva@gmail.com>
2021-06-10 19:29:59 -0300

    Remocao de ID do Esdras para evitar bloqueio devido ao excesso de uso.

--- lib/helper/cotacao_helper.dart
+++ lib/helper/cotacao_helper.dart
@@ -4,7 +4,7 @@ import 'dart:convert' as json;
 
 class CotacaoHelper {
   Future<Map> getCotacaoMoeda() async {
-    var uri = Uri.https('api.hgbrasil.com', 'finance', {'key':'363d755d'});
+    var uri = Uri.https('api.hgbrasil.com', 'finance', {'key':'SUA_ID'});
     //var uri = Uri.https('api.hgbrasil.com', 'finance');
     var response = await http.get(uri);
     return json.jsonDecode(response.body)['results']['currencies'];
